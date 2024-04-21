bde1aab95fd17e38329cab96d5ffca92af02675b
yuvikakathaith <yuvikakathaith3103@gmail.com>
2024-04-22 00:25:10 +0530

    update header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,29 @@
-import { Navbar, TextInput, Button, NavbarLink, NavbarCollapse } from 'flowbite-react';
+import { Avatar, Dropdown, Navbar, TextInput, Button, NavbarLink, NavbarCollapse } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
+import {useSelector} from 'react-redux'
 
 
 export default function Header() {
     const path = useLocation().pathname;
+    const {currentUser} = useSelector((state) => state.user);
+
+    const handleSignout = async () => {
+      try {
+        const res = await fetch('/api/user/signout', {
+          method: 'POST',
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data.message);
+        } else {
+          dispatch(signoutSuccess());
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
   return (
     <Navbar className='border-b-2'>
       <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-bold text-gray-800 dark:text-white">
@@ -29,6 +47,33 @@ export default function Header() {
         <Button className='w-12 h-10 sm:inline' color='gray' pill>
           <FaMoon />
         </Button>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
         <Link to='/sign-in' className='hidden sm:inline'>
             <Button outline gradientDuoTone='purpleToBlue'>
                 Sign In
