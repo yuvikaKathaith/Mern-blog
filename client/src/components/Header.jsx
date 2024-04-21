3729d0f9e21eb30eb97593ed24110ba4c1823953
yuvikakathaith <yuvikakathaith3103@gmail.com>
2024-04-22 00:44:23 +0530

    complete dark mode function

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,16 @@
 import { Avatar, Dropdown, Navbar, TextInput, Button, NavbarLink, NavbarCollapse } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
-import {useSelector} from 'react-redux'
+import { FaMoon, FaSun } from 'react-icons/fa';
+import {useSelector, useDispatch} from 'react-redux'
+import {toggleTheme} from '../redux/theme/themeSlice'
 
 
 export default function Header() {
     const path = useLocation().pathname;
     const {currentUser} = useSelector((state) => state.user);
-
+    const {theme} = useSelector((state) => state.theme);
+    const dispatch = useDispatch();
     const handleSignout = async () => {
       try {
         const res = await fetch('/api/user/signout', {
@@ -16,7 +18,7 @@ export default function Header() {
         });
         const data = await res.json();
         if (!res.ok) {
-          console.log(data.message);
+          console.log(data.message);  
         } else {
           dispatch(signoutSuccess());
         }
@@ -41,11 +43,12 @@ export default function Header() {
       </form>
       <Button className='w-12 h-10 lg:hidden' color='gray' pill>
           <AiOutlineSearch />
-      </Button>
+      </Button>   
       
       <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 sm:inline' color='gray' pill>
-          <FaMoon />
+        <Button className='w-12 h-10 sm:inline' color='gray' pill onClick={()=> dispatch(toggleTheme())}>
+
+          {theme === 'light'? <FaMoon/> : <FaSun/>}
         </Button>
         {currentUser ? (
           <Dropdown
