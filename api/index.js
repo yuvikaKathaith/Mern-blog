7f14a65577a27aa2d500e9ed2c5cb0ced8360615
yuvikakathaith <yuvikakathaith3103@gmail.com>
2024-04-22 19:44:07 +0530

    create a post API route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import postRoutes from './routes/post.route.js'
 import cookieParser from 'cookie-parser'
 
 
