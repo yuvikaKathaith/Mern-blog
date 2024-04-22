e6676ca110a0077142eac80dda912cc907f2c9d4
yuvikakathaith <yuvikakathaith3103@gmail.com>
2024-04-22 20:24:49 +0530

    add posts section to dashboard

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -151,7 +151,7 @@ export default function CreatePost() {
             setFormData({ ...formData, content: value });
           }}
         />
-        <Button type='submit' gradientDuoTone='purpleToPink'>
+        <Button type='submit' gradientDuoTone='purpleToBlue'>
           Publish
         </Button>
         {publishError && (
